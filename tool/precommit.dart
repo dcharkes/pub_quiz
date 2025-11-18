4c50ae1dbc07277b0536b708bf6752d0300ad9fc
Daco Harkes <dacoharkes@google.com>
2025-11-18 12:00:00 +0100

    add doc for the precommit hook

--- tool/precommit.dart
+++ tool/precommit.dart
@@ -1,3 +1,14 @@
+/// To set up this script as a pre-commit hook, create a file named `pre-commit`
+/// in the `.git/hooks/` directory with the following content:
+///
+/// ```
+/// #!/bin/sh
+/// dart tool/precommit.dart
+/// ```
+///
+/// Then, make the hook executable: chmod +x .git/hooks/pre-commit
+library;
+
 import 'dart:io';
 
 void main() {

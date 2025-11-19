fb35240e25d904bc1dede0b1971a9a33ba3488f6
Daco Harkes <dacoharkes@google.com>
2025-11-19 15:54:09 +0100

    play around with deployed serverpod

--- packages/pub_quiz_admin/web/main.dart
+++ packages/pub_quiz_admin/web/main.dart
@@ -6,6 +6,7 @@ import 'package:pub_quiz_client/pub_quiz_client.dart';
 void main() {
   const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
   final serverUrl = serverUrlFromEnv.isEmpty
+      // ? 'https://api.pubquiz.space/'
       ? 'http://localhost:8088/'
       : serverUrlFromEnv;
 
